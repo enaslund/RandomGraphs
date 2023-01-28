3b063dd5be3e36e8b852e13db1dc029d8759b61d
enaslund <naslund.eric@gmail.com>
2023-01-28 06:54:52 -0500

    Trying a test with os to see if the cloud computing will work

--- cyclic_covers.py
+++ cyclic_covers.py
@@ -1,3 +1,10 @@
+import os
+
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+
+
 import numpy as np
 import multiprocessing
 import time
